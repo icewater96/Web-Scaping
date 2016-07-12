fb4cd51d8b218e9c5d383c4c5fab4badfcbf74d0
Lu <JIULIU.LU@BECKMAN.COM>
2016-07-12 09:38:31 -0400

    'i'

--- demo.py
+++ demo.py
@@ -5,7 +5,14 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import time
+
+# This a client-side-generated webpage
 url = r'http://www.garnethill.com/sale-clearance/sale-of-the-day/#?p=Q&lbc=garnethill&uid=294457867&ts=custom&w=*&method=and&view=grid&af=cat2%3asaleclearance_saleoftheday%20cat1%3asaleclearance&isort=price'
+
+# Step 1: retrieve HTML
+# Step 2: HTML -> BeautifulSoup
+# or use Selenium handling HTML
 
 
 #%% Retrieve HTML from a URL
@@ -28,3 +35,26 @@ soup_1 = BeautifulSoup(html_1)
 soup_2 = BeautifulSoup(html_2)
 soup_3 = BeautifulSoup(urllib2.urlopen(url))
 
+
+#%% Scraping with selenium
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+br = webdriver.Firefox()
+
+# Headless
+#br = webdriver.PhantomJS()
+
+br.get( url )
+WebDriverWait( br, 200 ).until( EC.presence_of_element_located( (By.CLASS_NAME, "priceNow" ) ) )
+
+
+# br.page_source = HTML
+soup_4 = BeautifulSoup(br.page_source)
+
+all_items = soup_4.find_all( 'span', class_='priceNow')
+
+print
+br.close()
